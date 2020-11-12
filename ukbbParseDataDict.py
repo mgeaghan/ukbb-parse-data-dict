f0989a6dfa947f23950a88b16bc036c4fd73a4ec
mgeaghan <michael.p.geaghan@gmail.com>
2020-11-12 13:05:02 +1100

    fixed issues handling multiple tables and multiple lines of non-tabulated text

--- ukbbParseDataDict.py
+++ ukbbParseDataDict.py
@@ -1,7 +1,7 @@
 import html.parser as hp
 
 
-class ukbbTable():
+class ukbbData():
     def __checkInstance(self, objType):
         def isInst(x):
             return isinstance(x, objType)
@@ -72,18 +72,22 @@ class ukbbHtmlParser(hp.HTMLParser):
         self.rowspan = 1
 
     def updateUkbbData(self):
-        if isinstance(self.ukbbSection, ukbbTable):
+        if isinstance(self.ukbbSection, ukbbData):
             self.ukbbData.append(self.ukbbSection.makeDict())
 
     def handle_starttag(self, tag, attrs):
         if tag in ["h1", "h3"]:
-            self.newHeading = True
-            if not self.firstSection:  # push the previous section to the list of tables, unless this is the very first section
+            if not self.firstSection:  # when starting a new section, push the previous section to the list of tables, unless this is the very first section
                 self.updateUkbbData()
             else:
                 self.firstSection = False
+            self.ukbbSection = None  # erase the previous section, along with its heading and text
+            self.heading = ""
+            self.text = ""
+            self.newHeading = True
         elif tag == "table":
             self.newTable = True
+            self.tableDict = {}  # erase the previous table, along with its headers
             self.rowCount = 0
             self.tableHeaders = []  # keep track of the header labels
         elif self.newTable:
@@ -93,7 +97,7 @@ class ukbbHtmlParser(hp.HTMLParser):
             elif self.newRow:
                 if tag == "th":
                     self.newHeader = True
-                elif tag == "td":
+                elif self.tableHeaders != [] and tag == "td":
                     self.newData = True
                     if "rowspan" in [i[0] for i in attrs]:  # if a column's value spans over multiple rows, take not of how many
                         self.rowspan = int(attrs[[i[0] for i in attrs].index("rowspan")][1])
@@ -106,25 +110,27 @@ class ukbbHtmlParser(hp.HTMLParser):
 
     def handle_data(self, data):
         if self.newHeading:
-            self.heading = data
+            self.heading = data  # set the section heading
         elif self.newRow:
             if self.newHeader:
-                self.tableDict[data] = []
-                self.tableHeaders.append(data)
-            elif self.newData:
+                self.tableDict[data] = []  # create a new column
+                self.tableHeaders.append(data)  # set the new header name
+            elif self.tableHeaders != [] and self.newData:
                 h = self.tableHeaders[self.colCount]  # get header name for current column
                 if len(self.tableDict[h]) == self.rowCount - 1:  # if xth column for yth row hasn't been filled, fill it
                     self.tableDict[h].append(data)
                 elif len(self.tableDict[h]) == self.rowCount:  # if xth column for yth row has already been filled - this should only occur if there are additional tags in the cell, e.g. <a>'s - append the new data to the existing data
                     self.tableDict[h][-1] += "\n" + data
 
-        else:
-            self.text = data
+        else:  # accumulate any non-tabulated text in a string
+            if self.text == "":
+                self.text = data
+            else:
+                self.text += "\n" + data
 
     def handle_endtag(self, tag):
         if tag in ["h1", "h3"]:
             self.newHeading = False
-            self.ukbbSection = None
         elif tag == "tr":
             self.newRow = False
             self.rowCount += 1
@@ -135,11 +141,13 @@ class ukbbHtmlParser(hp.HTMLParser):
             self.colCount += 1
             self.newHeader = False
             self.newData = False
-        elif tag == "table":
+        elif tag == "table":  # once the end of a table is reached, add it to the current ukbbData object (or create one if it doesn't exist yet)
             if self.ukbbSection is None:
-                self.ukbbSection = ukbbTable(heading=self.heading, description=self.text, data=[self.tableDict])
+                self.ukbbSection = ukbbData(heading=self.heading, description=self.text, data=[self.tableDict])
             else:
                 self.ukbbSection.addData(self.tableDict)
+                self.ukbbSection.setDescription(self.text)  # update the non-tabulated text
             self.newTable = False
         elif tag == "html":
-            self.updateUkbbData()
+            self.ukbbSection.setDescription(self.text)  # update the non-tabulated text one last time
+            self.updateUkbbData()  # push the final section to the list of tables
